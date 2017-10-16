57b9f6e8566e602ee9280305490438e19944e78b
JIRASAK <boss.2587@hotmail.co.th>
2017-10-16 23:41:47 +0700

    first year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -10,7 +10,9 @@ namespace EngEdu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Year 255x semester 1, I am an Engedu freshman.")
+            Console.WriteLine("Year 255x semester 1, I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semester 2, I learn to program in c sharp.")
+           
         }
     }
 }
