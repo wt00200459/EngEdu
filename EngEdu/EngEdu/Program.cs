d94a176f95ad0a07b314a06ad28af84fbc042d9b
JIRASAK <boss.2587@hotmail.co.th>
2017-10-19 17:48:18 +0700

    add 4 year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,7 +13,11 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 1, I am an Engedu freshman.");
             Console.WriteLine("Year 255x semester 2, I learn to program in c sharp.");
             Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
+            Console.WriteLine("Year 2560 semester 2, I have to regrade on some course.");
+            Console.WriteLine("Year 2561 semester 1, I have to Grade A some course.");
+            Console.WriteLine("Year 2561 semester 2, I will go to teach student.");
            
+
         }
     }
 }
